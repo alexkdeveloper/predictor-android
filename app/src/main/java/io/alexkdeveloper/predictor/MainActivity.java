ab7e78fb7033814d5a9d2ab063ad410fd045dd75
Alex K <73014273+alexkdeveloper@users.noreply.github.com>
2024-01-16 22:34:37 +0800

    Add files via upload

--- app/src/main/java/io/alexkdeveloper/predictor/MainActivity.java
+++ app/src/main/java/io/alexkdeveloper/predictor/MainActivity.java
@@ -25,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
         text = findViewById(R.id.text);
         progress = findViewById(R.id.progress);
 
-        array = new String[]{"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes—definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.",
-                "Signs point to yes.", "Yep!", "Absolutely!", "You bet!", "Confirmed.", "Don’t count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.",
-                "Very doubtful.", "Naw.", "I’ve got a bad feeling about this…", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.",
-                "Concentrate and ask again.", "Impossible to see, the future is.", "404 Answer Not Found"};
+        array = new String[]{getString(R.string.it_is_certain), getString(R.string.it_is_decidedly_so), getString(R.string.without_a_doubt), getString(R.string.yes_definitely), getString(R.string.you_may_rely_on_it), getString(R.string.as_i_see_it_yes), getString(R.string.most_likely), getString(R.string.outlook_good), getString(R.string.yes),
+                getString(R.string.signs_point_to_yes), getString(R.string.yep), getString(R.string.absolutely), getString(R.string.you_bet), getString(R.string.confirmed), getString(R.string.don_t_count_on_it), getString(R.string.my_reply_is_no), getString(R.string.my_sources_say_no), getString(R.string.outlook_not_so_good),
+                getString(R.string.very_doubtful), getString(R.string.naw), getString(R.string.i_ve_got_a_bad_feeling_about_this), getString(R.string.reply_hazy_try_again), getString(R.string.ask_again_later), getString(R.string.better_not_tell_you_now), getString(R.string.cannot_predict_now),
+                getString(R.string.concentrate_and_ask_again), getString(R.string.impossible_to_see_the_future_is), getString(R.string._404_answer_not_found)};
 
         onButtonClicked(null);
     }
